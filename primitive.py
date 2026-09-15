cd8412bfff2b38c8f7211cb457436363423f622c
Nortoy <tolipovhusniddin2005@gmail.com>
2026-09-15 20:25:30 +0900

    feat: learn string in python

--- primitive.py
+++ primitive.py
@@ -1,4 +1,4 @@
-print("==============================")
+print("============= number =================")
 # in JAVA variables is a name storage location!
 # in Python, variables is named reference!
 
@@ -9,3 +9,20 @@ print(f"the count: {count} and type: {count_type}")
 result1 = count.bit_count() #method
 result2 = count.numerator #state
 print(result1, result2)
+
+
+
+print("============= string =================")
+
+course = "AI Python FullStack"
+result = type(course)
+print(f"the result (1): {result}")
+
+result = course.title()
+print(f"the result (2): {result}")
+
+result = course.upper()
+print(f"the result (3): {result}")
+
+result = course.replace("Fullstack", "Masterclass")
+print(f"the result (4): {result}")
