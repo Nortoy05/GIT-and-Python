4af9febf0f75dc1328c25a93b3e603495bbeb36b
Nortoy <tolipovhusniddin2005@gmail.com>
2026-09-15 20:12:21 +0900

    feat: python system variables & special methods via dunders

--- run.py
+++ run.py
@@ -1,1 +1,15 @@
+# Dunder ___builtins___, ___init___
+message = "Hello Brother"
+print(message)
 
+result = type(message)
+print("result:", result)
+
+
+''' in Python, there are builtin tools:
+(1) TYPES > int float str list dict
+(2) FUNCTIONS > print() len() input( ) type() str() int()
+(3) CONSTANTS > true false none
+'''
+
+print(dir(___builtins___))
